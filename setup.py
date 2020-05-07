657de59692e71fdcf7ff890fc22cdccbe27f4210
MtkN1XBt <51289448+MtkN1@users.noreply.github.com>
2020-05-07 20:31:13 +0900

    first commit

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from setuptools import setup
 
 setup(
     name='pybybit',
-    packages=['pybybit'],
+    packages=['pybybit', 'pybybit.util'],
     version='2.0.0',
     description='Bybit API client library for Python',
     author='MtkN1XBt',
     url='https://github.com/MtkN1/pybybit',
-    install_requires=['requests', 'websocket_client'],
-    keywords=['bitcoin', 'Bybit', 'API', 'Bot']
+    install_requires=['requests', 'websocket_client']
 )
